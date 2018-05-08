b672a9a7b1d18ff05a24b64b69c137ff60c5db6c
Maurits van Riezen <MauritsVanRiezen@gmail.com>
2018-05-08 15:30:32 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module : {
+    rules: [
+    {
+      test: /\.jsx$/,
+	  include: APP_DIR,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader'
+      }
+    }
+  ]
+  }
+};
+
+module.exports = config;
