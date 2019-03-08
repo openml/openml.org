9085cb55fb068e2b8b53c02f0020f601025bc1aa
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2019-03-08 14:43:15 +0100

    fixed routing for openml server

--- server.py
+++ server.py
@@ -5,4 +5,16 @@ app = Flask(__name__, static_url_path='', static_folder='src/client')
 app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
 
 if __name__ == '__main__':
+    app.run(port=int(os.environ.get("PORT", 8000)), debug=True)
+
+# Serve React App
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists("src/client/" + path):
+        return send_from_directory('src/client', path)
+    else:
+        return send_from_directory('src/client', 'index.html')
+
+if __name__ == '__main__':
     app.run(port=int(os.environ.get("PORT", 5000)), debug=True)
