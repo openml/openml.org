b822bc655181e9e15677c21cc9aa24268e2e9264
sahithyaravi <sahithyaravi1493@gmail.com>
2019-08-03 23:24:41 +0100

    simple test

--- src/dash/tests/test01.py
+++ src/dash/tests/test01.py
@@ -1,8 +1,13 @@
 from src.dash.dashapp import create_dash_app
 from flask import Flask
+import time
+def test_one(dash_br):
+    dash_br.server_url = 'http://127.0.0.1:5000/dashboard/'
+    assert dash_br.wait_for_element("h1").text == "Welcome to dash dashboard"
+    time.sleep(1)
+    assert dash_br.get_logs() == [], "browser console should contain no error"
 
-def test_one(dash_duo):
-    flask_app = Flask(__name__, static_url_path='', static_folder='src/client')
-    flask_app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
-    app = create_dash_app(flask_app)
-    dash_duo.start_server(app)
+    dash_br.percy_snapshot("dash testing page")
+# from selenium import webdriver
+#
+# driver = webdriver.Chrome()
