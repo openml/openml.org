cb0adcfa70c8fb5b7100d3d986ebdf6a2b4b6b66
sahithyaravi <sahithyaravi1493@gmail.com>
2019-08-05 10:28:39 +0100

    test with graph

--- src/dash/tests/test01.py
+++ src/dash/tests/test01.py
@@ -2,12 +2,14 @@ from src.dash.dashapp import create_dash_app
 from flask import Flask
 import time
 def test_one(dash_br):
-    dash_br.server_url = 'http://127.0.0.1:5000/dashboard/'
-    assert dash_br.wait_for_element("h1").text == "Welcome to dash dashboard"
-    time.sleep(1)
+    dash_br.server_url = 'http://127.0.0.1:5000/dashboard/data/5'
+    #assert dash_br.wait_for_element("h1").text == "Welcome to dash dashboard"
+    time.sleep(5)
+    print (dash_br.find_element("#distribution").text)
     assert dash_br.get_logs() == [], "browser console should contain no error"
 
     dash_br.percy_snapshot("dash testing page")
+
 # from selenium import webdriver
 #
 # driver = webdriver.Chrome()
