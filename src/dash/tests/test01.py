8510631b994aa40909879085faab927afa7ebafd
sahithyaravi1493 <sahithyaravi1493@gmail.com>
2019-08-06 15:27:18 +0200

    add table checks

--- src/dash/tests/test01.py
+++ src/dash/tests/test01.py
@@ -1,15 +1,14 @@
-from src.dash.dashapp import create_dash_app
-from flask import Flask
 import time
-def test_one(dash_br):
+from src.dash.helpers import get_data_metadata
+
+def test_data(dash_br):
+    data_id = 5
+    df, metadata, numerical_data, nominal_data, name = get_data_metadata(data_id)
     dash_br.server_url = 'http://127.0.0.1:5000/dashboard/data/5'
     #assert dash_br.wait_for_element("h1").text == "Welcome to dash dashboard"
-    time.sleep(5)
-    print (dash_br.find_element("#distribution").text)
+    time.sleep(20)
+    distribution_plot = dash_br.find_element("#distribution")
+    feature_table = dash_br.find_element("#datatable")
+    feature_importance = dash_br.find_element("#fi")
     assert dash_br.get_logs() == [], "browser console should contain no error"
-
-    dash_br.percy_snapshot("dash testing page")
-
-# from selenium import webdriver
-#
-# driver = webdriver.Chrome()
+    print(feature_table.text)
