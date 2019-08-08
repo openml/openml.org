5e96ae5c405817d1fafccb3ddcad0e26a022caf0
sahithyaravi <sahithyaravi1493@gmail.com>
2019-08-08 10:21:27 +0100

    fix error in test01

--- src/dash/tests/test01.py
+++ src/dash/tests/test01.py
@@ -1,8 +1,9 @@
 import time
 from src.dash.helpers import get_data_metadata
-
+import os
 def test_data(dash_br):
     data_id = 5
+    os.mkdir('cache')
     df, metadata, numerical_data, nominal_data, name = get_data_metadata(data_id)
     dash_br.server_url = 'http://127.0.0.1:5000/dashboard/data/5'
     #assert dash_br.wait_for_element("h1").text == "Welcome to dash dashboard"
@@ -12,3 +13,4 @@ def test_data(dash_br):
     feature_importance = dash_br.find_element("#fi")
     assert dash_br.get_logs() == [], "browser console should contain no error"
     print(feature_table.text)
+    print(metadata.to_string())
