a73ef2ef089841f3ee53fc61fe6cd14e677b5396
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2019-10-31 00:49:23 +0100

    catch-all routing

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import os
 from src.dashboard.dashapp import create_dash_app
 from flask import send_from_directory
 
-app = Flask(__name__, static_url_path='', static_folder='src/client/app/build',
+app = Flask(__name__, static_folder='src/client/app/build',
             instance_relative_config=True)
 app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
 #app.config.from_object('config')
@@ -17,11 +17,11 @@ create_dash_app(app)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
-    if path != "" and os.path.exists("src/client/app/build" + path):
-        return send_from_directory('src/client/app/build', path)
+    path_dir = os.path.abspath("src/client/app/build") #path react build
+    if path != "" and os.path.exists(os.path.join(path_dir, path)):
+        return send_from_directory(os.path.join(path_dir), path)
     else:
-        return send_from_directory('src/client/app/build', 'index.html')
-
+        return send_from_directory(os.path.join(path_dir),'index.html')
 
 if __name__ == '__main__':
     app.run(port=int(os.environ.get("PORT", 5000)), debug=True, ssl_context='adhoc')
