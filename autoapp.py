b3f4cd1c0e1a5f04d0ddca90b7e9be1e4d137e54
Prabhant Singh <prabhantsingh@gmail.com>
2019-11-27 12:57:32 +0100

    ignore

--- autoapp.py
+++ autoapp.py
@@ -3,3 +3,4 @@
 from server.app import create_app
 
 app = create_app()
+app.secret_key = 'abcd'
--- server/user/views.py
+++ server/user/views.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from server.user.models import User
-blueprint = Blueprint("user", __name__, url_prefix=" ", static_folder="'src/client/app/build'")
+blueprint = Blueprint("user", __name__)
 
-@blueprint.route('/signup', methods=['POST', 'GET'])
+@blueprint.route('/profile', methods=['POST', 'GET'])
 @login_required
 def profile():
     print('profile executed')
