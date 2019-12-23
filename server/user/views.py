1d2d5a98f9b8679d175a1e455a4d7947b2d22dc7
Prabhant Singh <prabhantsingh@gmail.com>
2019-12-23 17:44:48 +0100

    json user

--- server/user/views.py
+++ server/user/views.py
@@ -41,10 +41,11 @@ def login():
 @jwt_required
 def profile():
     current_user = get_jwt_identity()
+    user = User.query.filter_by(email=current_user).first()
     if request.method == 'GET':
         print(current_user)
         print('profile executed')
-        return "current_user"
+        return jsonify({"username":user.username, "bio":user.bio, "first_name":user.first_name, "last_name":user.last_name, "email":user.email}), 200
     else:
         return "post user"
 
