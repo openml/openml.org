dd42c2ea08101b4e037a8fc6d0ad9f769daf9282
Prabhant Singh <prabhantsingh@gmail.com>
2020-01-10 11:31:18 +0100

    DB initial integration
    Via sqlalchemy and BASE decalarative method

--- server/public/views.py
+++ server/public/views.py
@@ -9,7 +9,7 @@ from flask import (
 )
 from flask_login import login_required, login_user, logout_user
 from server.user.models import User
-from server.extensions import login_manager, db
+from server.extensions import db
 
 blueprint = Blueprint("public", __name__)
 
@@ -29,8 +29,29 @@ def signupfunc():
         robj = request.get_json()
         user = User(username=robj['name'], email=robj['email'])
         user.set_password(robj['password'])
-        # max_id = db.session.query(db.func.max(User.id)).scalar()
-        # user.set_user_id(max_id)
+        user.ip_address='0.00.'
+        user.activation_selector = '0000'
+        user.activation_code = '0000'
+        user.forgotten_password_selector = '0000'
+        user.forgotten_password_code = '0000'
+        user.forgotten_password_time ='0000'
+        user.remember_selector = '0000'
+        user.remember_code = '0000'
+        user.created_on = '0000'
+        user.last_login = '0000'
+        user.active = '0000'
+        user.first_name = '0000'
+        user.last_name = '0000'
+        user.company = '0000'
+        user.phone ='0000'
+        user.country = '0000'
+        user.image = '0000'
+        user.bio = '0000'
+        user.core = '0000'
+        user.external_source ='0000'
+        user.external_id = '0000'
+        user.session_hash = '0000'
+        user.password_hash ='0000'
         db.session.add(user)
         db.session.commit()
         print('signedup')
@@ -44,5 +65,4 @@ def signupfunc():
 @blueprint.route('/logout')
 @login_required
 def logout():
-    logout_user()
     return redirect(url_for('index'))
--- server/user/models.py
+++ server/user/models.py
@@ -36,18 +36,15 @@ class User(Base):
     # password_hash = Column(String(120))
 
 
-    # def set_password(self, password):
-    #     self.password_hash = argon2.generate_password_hash(password)
+    def set_password(self, password):
+        self.password = argon2.generate_password_hash(password)
     #
-    # def check_password(self, password):
-    #     #password = argon2.generate_password_hash(password)
-    #     return argon2.check_password_hash(self.password_hash, password)
+    def check_password(self, passwd):
+        #password = argon2.generate_password_hash(password)
+        return argon2.check_password_hash(self.password, passwd)
+
     #
-    # def get_id(self):
-    #     print('called')
-    #     return self.email
-    #
-    # def __repr__(self):
-    #     return '<User {}>'.format(self.username)
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
 
 
