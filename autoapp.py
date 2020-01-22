3a1a7c6691f515c6f1f06b76c217af32eef61b93
sahithyaravi1493 <sahithyaravi1493@gmail.com>
2020-01-22 14:05:24 +0100

    fix routing of all routes flask

--- autoapp.py
+++ autoapp.py
@@ -1,10 +1,37 @@
-import os
-# -*- coding: utf-8 -*-
-"""Create an application instance."""
-from server.app import create_app
+from server.app import *
+from flask import jsonify
 
-app = create_app()
+# Create flask app and routing
+app = Flask(__name__, static_url_path='', static_folder='server/src/client/app/build',
+            instance_relative_config=True)
+app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
+CORS(app)
+app.config.from_object(Config)
+register_extensions(app)
+register_blueprints(app)
+jwt = JWTManager(app)
 app.secret_key = 'abcd'
+
+
+@app.route('/', defaults={'path': ''})
+@app.route("/<path:path>")
+def serve_static(path):
+    """ Never ever reached! """
+    print("static folder serve")
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # This seems to catch all routes! Refer link:
+    # https://stackoverflow.com/questions/14023864/flask-url-route-route-all-other-urls-to-some-function
+    print("error handler reached!")
+    print(request.url)
+    return send_from_directory(app.static_folder, "index.html")
+
 
 if __name__ == '__main__':
     app.run(port=int(os.environ.get("PORT", 5000)), debug=True, ssl_context='adhoc')
