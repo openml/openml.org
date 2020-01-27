ea7a6ac30d21ed4929059bca082b26aec5537ad8
PGijsbers <p.gijsbers@tue.nl>
2020-01-27 17:00:42 +0100

    Style: correct white spacing.

--- server/src/dashboard/helpers.py
+++ server/src/dashboard/helpers.py
@@ -146,13 +146,14 @@ def splitDataFrameList(df, target_column):
     returns: a dataframe with each entry for the target column separated, with each element moved into a new row.
     The values in the other columns are duplicated across the newly divided rows.
     """
-    def splitListToRows(row,row_accumulator, target_column):
+    def splitListToRows(row, row_accumulator, target_column):
         split_row = row[target_column]
         for s in split_row:
             new_row = row.to_dict()
             new_row[target_column] = s
             row_accumulator.append(new_row)
     new_rows = []
-    df.apply(splitListToRows,axis=1,args = (new_rows,target_column))
+    df.apply(splitListToRows, axis=1, args=(new_rows, target_column))
     new_df = pd.DataFrame(new_rows)
     return new_df
+
