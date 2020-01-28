ed45f0a458215886e2951ba628cbc7f5de66bc94
PGijsbers <p.gijsbers@tue.nl>
2020-01-28 14:47:17 +0100

    Add mean to scatterplot of folds. Currently mean independent color from folds.

--- server/src/dashboard/study_callbacks.py
+++ server/src/dashboard/study_callbacks.py
@@ -50,8 +50,15 @@ def register_study_callbacks(app):
 
             for i, (flow_name, flow_df) in enumerate(df.groupby('flow_name')):
                 y_offset = i * dy - dy_range / 2
-                ys = [dataset_y_map[d] + y_offset for d in flow_df['data_name']]
-                fig.add_trace(go.Scatter(x=flow_df['values'], y=ys, mode='markers', name=flow_name, opacity=0.75))
+                # Individual fold results:
+                y_folds = [dataset_y_map[d] + y_offset for d in flow_df['data_name']]
+                fold_trace = go.Scatter(x=flow_df['values'], y=y_folds, mode='markers', name=f'{flow_name}_fold', opacity=0.75, legendgroup=flow_name, showlegend=False)
+                fig.add_trace(fold_trace)
+                # Mean performance:
+                flow_mean_df = flow_df.groupby('data_name', as_index=False).mean()
+                y_mean = [dataset_y_map[d] + y_offset for d in flow_mean_df['data_name']]
+                mean_trace = go.Scatter(x=flow_mean_df['values'], y=y_mean, mode='markers', marker=dict(symbol='diamond'), legendgroup=flow_name, name=flow_name)
+                fig.add_trace(mean_trace)
 
             def create_link_html(data_name: str, dataset_id: str, character_limit: int = 15) -> str:
                 """ Return a html hyperlink (<a>) to the dataset page, text is shortened to `character_limit` chars. """
