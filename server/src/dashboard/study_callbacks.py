ffc766664994f8593ae7cfeeb4313197e9dcaea0
PGijsbers <p.gijsbers@tue.nl>
2020-01-29 16:13:09 +0100

    Add hover text.

--- server/src/dashboard/study_callbacks.py
+++ server/src/dashboard/study_callbacks.py
@@ -46,16 +46,28 @@ def register_study_callbacks(app):
             for i, (flow_name, flow_df) in enumerate(df.groupby('flow_name')):
                 # See https://plot.ly/python/discrete-color/#color-sequences-in-plotly-express for color palettes
                 flow_color = px.colors.qualitative.Plotly[i]
+
+                shared_template = (f'{metric}: ' + '%{x}<br>'
+                                   f'Flow: {flow_name}<br>'
+                                   '%{text}<br>'
+                                   '<extra></extra>')  # Removes a second box with trace information
+
                 y_offset = i * dy - dy_range / 2
                 # Individual fold results:
+                fold_text = [(f'Dataset: {dataset_name}<br>'
+                              'Fold score') for dataset_name in flow_df['data_name']]
                 y_folds = [dataset_y_map[d] + y_offset for d in flow_df['data_name']]
                 fold_trace = go.Scatter(x=flow_df['values'], y=y_folds, mode='markers', name=f'{flow_name}_fold',
-                                        opacity=0.5, legendgroup=flow_name, showlegend=False, marker=dict(color=flow_color))
+                                        opacity=0.5, legendgroup=flow_name, showlegend=False, marker=dict(color=flow_color),
+                                        text=fold_text, hovertemplate=shared_template)
                 fig.add_trace(fold_trace)
+
                 # Mean performance:
                 flow_mean_df = flow_df.groupby('data_name', as_index=False).mean()
                 y_mean = [dataset_y_map[d] + y_offset for d in flow_mean_df['data_name']]
-                mean_trace = go.Scatter(x=flow_mean_df['values'], y=y_mean, mode='markers', marker=dict(symbol='diamond', color=flow_color), legendgroup=flow_name, name=flow_name)
+                mean_text = [(f'Dataset: {dataset_name}<br>'
+                              'Mean score') for dataset_name in flow_mean_df['data_name']]
+                mean_trace = go.Scatter(x=flow_mean_df['values'], y=y_mean, mode='markers', marker=dict(symbol='diamond', color=flow_color), legendgroup=flow_name, name=flow_name, text=mean_text, hovertemplate=shared_template)
                 fig.add_trace(mean_trace)
 
             def create_link_html(data_name: str, dataset_id: str, character_limit: int = 15) -> str:
