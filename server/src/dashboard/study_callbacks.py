56838cc314597dc095ef820c009732e7d7ca68c2
PGijsbers <p.gijsbers@tue.nl>
2020-01-29 15:31:59 +0100

    Match trace color of folds and means.

--- server/src/dashboard/study_callbacks.py
+++ server/src/dashboard/study_callbacks.py
@@ -5,7 +5,7 @@ import dash_core_components as dcc
 from .helpers import *
 import re
 import openml
-import time
+import plotly.express as px
 
 
 def register_study_callbacks(app):
@@ -44,15 +44,18 @@ def register_study_callbacks(app):
             dy = dy_range/(n_flows - 1)  # Distance between individual flows
 
             for i, (flow_name, flow_df) in enumerate(df.groupby('flow_name')):
+                # See https://plot.ly/python/discrete-color/#color-sequences-in-plotly-express for color palettes
+                flow_color = px.colors.qualitative.Plotly[i]
                 y_offset = i * dy - dy_range / 2
                 # Individual fold results:
                 y_folds = [dataset_y_map[d] + y_offset for d in flow_df['data_name']]
-                fold_trace = go.Scatter(x=flow_df['values'], y=y_folds, mode='markers', name=f'{flow_name}_fold', opacity=0.75, legendgroup=flow_name, showlegend=False)
+                fold_trace = go.Scatter(x=flow_df['values'], y=y_folds, mode='markers', name=f'{flow_name}_fold',
+                                        opacity=0.5, legendgroup=flow_name, showlegend=False, marker=dict(color=flow_color))
                 fig.add_trace(fold_trace)
                 # Mean performance:
                 flow_mean_df = flow_df.groupby('data_name', as_index=False).mean()
                 y_mean = [dataset_y_map[d] + y_offset for d in flow_mean_df['data_name']]
-                mean_trace = go.Scatter(x=flow_mean_df['values'], y=y_mean, mode='markers', marker=dict(symbol='diamond'), legendgroup=flow_name, name=flow_name)
+                mean_trace = go.Scatter(x=flow_mean_df['values'], y=y_mean, mode='markers', marker=dict(symbol='diamond', color=flow_color), legendgroup=flow_name, name=flow_name)
                 fig.add_trace(mean_trace)
 
             def create_link_html(data_name: str, dataset_id: str, character_limit: int = 15) -> str:
