954ea21cf918355733c064f2b4e91df185bd1df5
sahithyaravi1493 <sahithyaravi1493@gmail.com>
2020-02-03 13:07:25 +0100

    update requirememts

--- autoapp.py
+++ autoapp.py
@@ -33,4 +33,4 @@ def page_not_found(e):
 
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT", 5000)), debug=True, ssl_context='adhoc')
+    app.run(port=int(os.environ.get("PORT", 5000)), debug=True)
