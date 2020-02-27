cd3e5cf8e9666b4f7eda8fd9e26454b55ae1afba
sahithyaravi1493 <sahithyaravi1493@gmail.com>
2020-02-27 11:26:34 +0100

    restructure code

--- server/src/dashboard/dashapp.py
+++ server/src/dashboard/dashapp.py
@@ -4,18 +4,19 @@ import shutil
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+
 from flask_caching import Cache
 from .callbacks import register_callbacks
 
-# To do: Move to assets (Copied from Joaquin's react font)
-font = ["Nunito Sans", "-apple-system", "BlinkMacSystemFont", '"Segoe UI"', "Roboto", '"Helvetica Neue"',
-        "Arial", "sans-serif", '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"']
+# TODO: Move to assets (Copied from Joaquin's react font)
+font = ["Nunito Sans", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue",
+        "Arial", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"]
 
 
 def create_dash_app(flask_app):
-    """Create a dash application with same server as given flask app.
-
-    :param flask_app: This dash app runs on the same server as flask_app
+    """
+    Create dash application
+    :param flask_app: flask_app passed is the flask server for the dash app
     :return:
     """
 
@@ -33,7 +34,7 @@ def create_dash_app(flask_app):
     # 2. Page content - loaded based on URL path
     page_content = html.Div(id='page-content', style={"fontFamily": font, 'background-color': 'white'})
     # 3. Loading icon
-    global_loading_icon = dcc.Loading(html.Div(id='loading-indictor',
+    global_loading_icon = dcc.Loading(html.Div(id='loading-indicator',
                                                style={'display': 'none'}),
                                       type='dot')
     app.layout = html.Div([url,
@@ -43,7 +44,6 @@ def create_dash_app(flask_app):
     # Callbacks
     register_callbacks(app, cache)
 
-    print("Admin access may be required to create the cache directory")
     # Create a temporary cache for data transfer between callbacks - pkl files
     shutil.rmtree('cache', ignore_errors=True)
     os.system('sudo mkdir cache')
