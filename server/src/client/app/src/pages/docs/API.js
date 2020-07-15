0bc613b86ceafd56b5499e4e60f6a4a9d09c2e69
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2020-07-15 15:15:01 +0200

    cleaner interface

--- server/src/client/app/src/pages/docs/API.js
+++ server/src/client/app/src/pages/docs/API.js
@@ -20,6 +20,68 @@ const StyledSwaggerUI = styled.div`
   [id^="model-"][id*="Trace"] {
     display: none;
   }
+  .swagger-ui .scheme-container {
+    background: inherit;
+    box-shadow: none;
+  }
+  .swagger-ui section.models .model-container {
+    background: inherit;
+  }
+  .swagger-ui .info {
+    margin-bottom: 0px;
+  }
+  .swagger-ui .servers > label select {
+    height: 40px;
+  }
+  .swagger-ui table.model tr.property-row td {
+    padding: 0.6em;
+    font-size: 12pt;
+  }
+  .swagger-ui table.model tr.property-row td:first-child {
+    padding: 0.6em;
+    font-size: 12pt;
+  }
+  .swagger-ui td .model .pointer .model-title {
+    display: none;
+  }
+  .swagger-ui td .model-toggle {
+    font-size: 20px;
+    top: 2px;
+    margin-left: 0px;
+  }
+  .swagger-ui td .brace-open {
+    font-size: 16px;
+  }
+  .renderedMarkdown {
+    color: #999;
+  }
+  .swagger-ui .opblock-tag {
+    margin: 0px;
+    background-color: #fff;
+    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+      0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
+  }
+  .no-margin {
+    display: flex;
+    flex-direction: column;
+  }
+  .swagger-ui .opblock-get {
+    padding-left: 5px;
+    margin-bottom: 1px;
+  }
+  .swagger-ui .opblock-post {
+    padding-left: 5px;
+    margin-bottom: 1px;
+    order: 2;
+  }
+  .swagger-ui .opblock-delete {
+    padding-left: 5px;
+    margin-bottom: 1px;
+    order: 3;
+  }
+  .swagger-ui .opblock-summary {
+    height: 50px;
+  }
 `;
 
 function OpenMLSwaggerUI() {
