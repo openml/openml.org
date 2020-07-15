84a8ae924b17f40ffc7f70efb7b8aba681f1de2e
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2020-07-15 13:13:06 +0200

    rest api improvements

--- server/src/client/app/src/pages/docs/API.js
+++ server/src/client/app/src/pages/docs/API.js
@@ -1,9 +1,33 @@
 import React from "react";
+import styled from "styled-components";
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
+import "swagger-ui-themes/themes/3.x/theme-material.css";
+
+const StyledSwaggerUI = styled.div`
+  [id^="model-"][id*="_"] {
+    display: none;
+  }
+  [id^="model-"][id*="List"] {
+    display: none;
+  }
+  [id^="model-"][id*="Unprocessed"] {
+    display: none;
+  }
+  [id^="model-"][id*="Request"] {
+    display: none;
+  }
+  [id^="model-"][id*="Trace"] {
+    display: none;
+  }
+`;
 
 function OpenMLSwaggerUI() {
-  return <SwaggerUI url="swagger.json" />;
+  return (
+    <StyledSwaggerUI>
+      <SwaggerUI url="swagger.json" />
+    </StyledSwaggerUI>
+  );
 }
 
 export default OpenMLSwaggerUI;
