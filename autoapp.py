9f5ba3cc916709f233df4b4617496e4ff0c32032
prabhantsingh@gmail.com <prabhantsingh@gmail.com>
2020-08-04 20:39:17 +0200

    URL Redirection

--- autoapp.py
+++ autoapp.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, redirect
 from flask_cors import CORS
 
 from server.app import register_blueprints, register_extensions
@@ -24,6 +24,7 @@ def serve_static(path):
     """ Never ever reached! """
     print("static folder serve")
     if path != "" and os.path.exists(app.static_folder + '/' + path):
+        print(path)
         return send_from_directory(app.static_folder, path)
     else:
         return send_from_directory(app.static_folder, 'index.html')
@@ -35,6 +36,37 @@ def page_not_found(e):
     # https://stackoverflow.com/questions/14023864/flask-url-route-route-all-other-urls-to-some-function
     print("error handler reached!")
     print(request.url)
+    if '/d/' in request.url:
+        print('Data in URL')
+        url = str(request.url)
+        url = url.split('/d/')
+        print(url[1])
+        return redirect(os.environ.get('REDIRECT_URL')+'/search?type=data&sort=runs&id='+url[1])
+    elif '/t/' in request.url:
+        print('Task in URL')
+        url = str(request.url)
+        url = url.split('/t/')
+        print(url[1])
+        return redirect(os.environ.get('REDIRECT_URL')+'/search?type=task&sort=runs&id='+url[1])
+    elif '/f/' in request.url:
+        print('Flow in URL')
+        url = str(request.url)
+        url = url.split('/f/')
+        print(url[1])
+        return redirect(os.environ.get('REDIRECT_URL')+'/search?type=flow&sort=runs&id='+url[1])
+    elif '/r/' in request.url:
+        print('Run in URL')
+        url = str(request.url)
+        url = url.split('/r/')
+        print(url[1])
+        return redirect(os.environ.get('REDIRECT_URL')+'/search?type=run&sort=date&id='+url[1])
+    elif '/s/' in request.url:
+        print('Study in URL')
+        url = str(request.url)
+        url = url.split('/s/')
+        print(url[1])
+        return redirect(os.environ.get('REDIRECT_URL')+'/search?type=study&study_type=run&id='+url[1])
+
     return send_from_directory(app.static_folder, "index.html")
 
     # return app
