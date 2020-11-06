d2aee74aec9c30769329d345bc7455577d5b7865
prabhantsingh@gmail.com <prabhantsingh@gmail.com>
2020-11-06 16:38:03 +0100

    data edit

--- server/data/views.py
+++ server/data/views.py
@@ -9,79 +9,133 @@ import openml
 import pandas as pd
 import json
 import arff
+from urllib.parse import parse_qs, urlparse
+import pdb 
 
 data_blueprint = Blueprint("data", __name__, static_folder='server/src/client/app/build')
 
 CORS(data_blueprint)
 
 
-@data_blueprint.route('/data-edit', methods=['POST'])
+@data_blueprint.route("/data-edit", methods=['GET', 'POST'])
 @jwt_required
 def data_edit():
     current_user = get_jwt_identity()
     user = User.query.filter_by(email=current_user).first()
     openml.config.apikey = user.session_hash
-    # TODO change line below in development\
     testing = os.environ.get('TESTING')
     if testing:
         openml.config.start_using_configuration_for_example()
-    j_obj = request.get_json()
-    dataset_id = j_obj['dataset_id']
-    dataset_id = int(dataset_id)
-    # dataset = openml.datasets.get_dataset(int(dataset_id))
-    description = j_obj['description']
-    creator = j_obj['creator']
-    collection_date = j_obj['date']
-    citation = j_obj['citation']
-    language = j_obj['language']
-    if description == '':
-        description = None
-    if creator == '':
-        creator = None
-    if collection_date == '':
-        collection_date = None
-    if citation == '':
-        citation = None
-    if language == '':
-        language = None
-    data_id = openml.datasets.edit_dataset(dataset_id,
-                                           description=description,
-                                           creator=creator,
-                                           collection_date=collection_date,
-                                           citation=citation,
-                                           language=language)
+        url = request.args.get('url')
+    parsed = urlparse(url)
+    dataset_id = parse_qs(parsed.query)['id']
+    dataset_id = int(dataset_id[0])
+    print(dataset_id)
+    if request.method == 'GET':
 
-    return str(data_id)
+        dataset_info = openml.datasets.list_datasets([dataset_id])
+        dataset_info = list(dataset_info.items())
+        # Its a nested ordered dictionary the first index is to get id, second to get info and third for upload
+        uploader_id = dataset_info[0][1]['uploader']
+        owner = 'false'
+        dataset = openml.datasets.get_dataset(dataset_id)
+        if (int(user.id) == int(uploader_id)):
+            owner = 'true'
+        print(jsonify({"user_id": user.id,
+                       "description": dataset.description, "creator": dataset.creator, "date": dataset.collection_date,
+                       "citation": dataset.citation, "language": dataset.language,
+                       "default_target_attribute": dataset.default_target_attribute,
+                       "ignore_attribute": dataset.ignore_attribute,
+                       "row_id_attribute": dataset.row_id_attribute, "owner": owner}))
+        return jsonify({"user_id": user.id,
+                        "description": dataset.description, "creator": dataset.creator, "date": dataset.collection_date,
+                        "citation": dataset.citation, "language": dataset.language,
+                        "default_target_attribute": dataset.default_target_attribute,
+                        "ignore_attribute": dataset.ignore_attribute,
+                        "row_id_attribute": dataset.row_id_attribute, "owner": owner}), 200
+    elif request.method == "POST":
+        j_obj = request.get_json()
+        # dataset = openml.datasets.get_dataset(int(dataset_id))
+        owner = j_obj['owner']
+        description = j_obj['description']
+        creator = j_obj['creator']
+        collection_date = j_obj['date']
+        citation = j_obj['citation']
+        language = j_obj['language']
+        if description == '':
+            description = None
+        if creator == '':
+            creator = None
+        if collection_date == '':
+            collection_date = None
+        if citation == '':
+            citation = None
+        if language == '':
+            language = None
+        if owner == 'false':
+            data_id = openml.datasets.edit_dataset(dataset_id,
+                                                   description=description,
+                                                   creator=creator,
+                                                   collection_date=collection_date,
+                                                   citation=citation,
+                                                   language=language)
+        elif owner == 'true':
+            default_target_attribute = j_obj['default_target_attribute']
+            ignore_attribute = j_obj['ignore_attribute']
+            row_id_attribute = j_obj['row_id_attribute']
+            if default_target_attribute == '':
+                default_target_attribute = None
+            if ignore_attribute == '':
+                ignore_attribute = None
+            if row_id_attribute == '':
+                row_id_attribute = None
+            try:
+                data_id = openml.datasets.edit_dataset(dataset_id,
+                                                       description=description,
+                                                       creator=creator,
+                                                       collection_date=collection_date,
+                                                       citation=citation,
+                                                       language=language,
+                                                       default_target_attribute=default_target_attribute,
+                                                       ignore_attribute=ignore_attribute,
+                                                       row_id_attribute=row_id_attribute)
+            except:
+                return 'error'
+                # print(e)
+                # if e.code == 1065:
+                #     return e
+
+        return str('data edit successful')
 
 
-@data_blueprint.route('/data-edit-critical', methods=['POST'])
-@jwt_required
-def data_edit_critical():
-    current_user = get_jwt_identity()
-    user = User.query.filter_by(email=current_user).first()
-    openml.config.apikey = user.session_hash
-    # TODO change line below in development\
-    testing = os.environ.get('TESTING')
-    if testing:
-        openml.config.start_using_configuration_for_example()
-    j_obj = request.get_json()
-    dataset_id = j_obj['dataset_id']
-    dataset_id = int(dataset_id)
-    default_target_attribute = j_obj['default_target_attribute']
-    ignore_attribute = j_obj['ignore_attribute']
-    row_id_attribute = j_obj['row_id_attribute']
-    if default_target_attribute == '':
-        default_target_attribute = None
-    if ignore_attribute == '':
-        ignore_attribute = None
-    if row_id_attribute == '':
-        row_id_attribute = None
-    data_id = openml.datasets.edit_dataset(dataset_id,
-                                           default_target_attribute=default_target_attribute,
-                                           ignore_attribute=ignore_attribute,
-                                           row_id_attribute=row_id_attribute
-                                           )
-    return str(data_id)
+# @data_blueprint.route('/data-edit-critical', methods=['POST'])
+# @jwt_required
+# def data_edit_critical():
+#     current_user = get_jwt_identity()
+#     user = User.query.filter_by(email=current_user).first()
+#     openml.config.apikey = user.session_hash
+#     # TODO change line below in development\
+#     testing = os.environ.get('TESTING')
+#     if testing:
+#         openml.config.start_using_configuration_for_example()
+#     j_obj = request.get_json()
+#     dataset_id = j_obj['dataset_id']
+#     dataset_id = int(dataset_id)
+#     default_target_attribute = j_obj['default_target_attribute']
+#     ignore_attribute = j_obj['ignore_attribute']
+#     row_id_attribute = j_obj['row_id_attribute']
+#     if default_target_attribute == '':
+#         default_target_attribute = None
+#     if ignore_attribute == '':
+#         ignore_attribute = None
+#     if row_id_attribute == '':
+#         row_id_attribute = None
+#     data_id = openml.datasets.edit_dataset(dataset_id,
+#                                            default_target_attribute=default_target_attribute,
+#                                            ignore_attribute=ignore_attribute,
+#                                            row_id_attribute=row_id_attribute
+#                                            )
+#     return str(data_id)
 
 
 @data_blueprint.route('/data-upload', methods=['POST'])
--- server/src/client/app/src/pages/auth/DataEdit.js
+++ server/src/client/app/src/pages/auth/DataEdit.js
@@ -12,7 +12,7 @@ import {
     TextField,
     Typography
 } from "@material-ui/core";
-
+import Box from '@material-ui/core/Box';
 import {spacing} from "@material-ui/system";
 import {Redirect} from "react-router-dom";
 
@@ -23,41 +23,104 @@ const Divider = styled(MuiDivider)(spacing);
 const FormControl = styled(MuiFormControl)(spacing);
 
 function Public() {
+    const [description, setDescription] = useState("");
+    const [creator, setCreator] = useState("");
+    const [date, setDate] = useState("");
+    const [citation, setCitation] = useState("");
+    const [language, setLanguage] = useState("");
+    const [def_tar_att, setDefTatt] = useState("");
+    const [row_id_att, setRowIdatt] = useState("");
+    const [ignore_att, setIgnoreatt] = useState("");
     const [error, setError] = useState(false);
-    //todo set error messages for task uploads
     const [errormessage, setErrorMessage] = useState(false);
     const [success, setSuccess] = useState(false);
-
+    const [user_id, setUserId] = useState("");
+    const [owner, setOwner] = useState(false);
     const yourConfig = {
         headers: {
             Authorization: "Bearer " + localStorage.getItem("token")
-        }
+        },
+        params: {
+            url: window.location.href,
+        },
+
+
     };
+
+    axios
+        .get(process.env.REACT_APP_SERVER_URL + "data-edit", yourConfig)
+        .then(function (response) {
+            console.log(response.data);
+            setUserId(response.data.user_id);
+            setDescription(response.data.description);
+            setCreator(response.data.creator);
+            setDate(response.data.date);
+            setLanguage(response.data.language);
+            setCitation(response.data.citation);
+            setDefTatt(response.data.default_target_attribute);
+            setRowIdatt(response.data.row_id_attribute);
+            setIgnoreatt(response.data.ignore_attribute);
+            if (response.data.owner === 'true') {
+                setOwner(true);
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 
     function datatoflask(event) {
         event.preventDefault();
+        if (owner === true) {
+            axios
+                .post(
+                    process.env.REACT_APP_SERVER_URL + "data-edit",
+                    {
+                        owner: "true",
+                        description: event.target.description.value,
+                        creator: event.target.creator.value,
+                        date: event.target.collection_date.value,
+                        citation: event.target.citation.value,
+                        language: event.target.language.value,
+                        default_target_attribute: event.target.default_target_attribute.value,
+                        ignore_attribute: event.target.ignore_attribute.value,
+                        row_id_attribute: event.target.row_id_attribute.value
+                    },
+                    yourConfig
+                )
+                .then(function (response) {
+                    console.log(response.data);
+                })
+                .catch(function (error) {
+                    setError(true);
+                    setErrorMessage("Cannot edit");
+                    console.log(error.data);
+                });
+        }
+        else if(owner === false)
+        {
+            axios
+                .post(
+                    process.env.REACT_APP_SERVER_URL + "data-edit",
+                    {
+                        owner: "false",
+                        description: event.target.description.value,
+                        creator: event.target.creator.value,
+                        date: event.target.collection_date.value,
+                        citation: event.target.citation.value,
+                        language: event.target.language.value,
+                    },
+                    yourConfig
+                )
+                .then(function (response) {
+                    console.log(response.data);
+                })
+                .catch(function (error) {
+                    setError(true);
+                    setErrorMessage("Cannot edit");
+                    console.log(error.data);
+                });
 
-        axios
-            .post(
-                process.env.REACT_APP_SERVER_URL + "data-edit",
-                {
-                    dataset_id: event.target.dataset_id.value,
-                    description: event.target.description.value,
-                    creator: event.target.creator.value,
-                    date: event.target.collection_date.value,
-                    citation: event.target.citation.value,
-                    language: event.target.language.value
-                },
-                yourConfig
-            )
-            .then(function (response) {
-                console.log(response.data);
-            })
-            .catch(function (error) {
-                setError(true);
-                setErrorMessage("Cannot edit");
-                console.log(error.data);
-            });
+        }
 
         return false;
     }
@@ -76,39 +139,57 @@ function Public() {
                 <Grid container spacing={6}>
                     <Grid item md={8}>
                         <FormControl fullWidth mb={3}>
-                            <InputLabel htmlFor="dataset_id">Dataset id</InputLabel>
-                            <Input id="dataset_id"/>
-                        </FormControl>
-
-                        <FormControl fullWidth mb={3}>
                             <TextField
                                 label="Description"
                                 id="description"
                                 rows={3}
+                                multiline={true}
                                 rowsMax={4}
+                                defaultValue={description}
                             />
                         </FormControl>
                         <FormControl fullWidth mb={3}>
                             <InputLabel htmlFor="citation">Citation</InputLabel>
-                            <Input id="citation" placeholder="citation"/>
+                            <Input id="citation" placeholder="citation" defaultValue={citation} multiline={true}/>
                         </FormControl>
                         <FormControl fullWidth mb={3}>
-                            <TextField label="Creator" id="creator" rows={3} rowsMax={4}/>
-                        </FormControl>
-                        <FormControl fullWidth mb={3}>
-                            <InputLabel htmlFor="contributor">Contributor(s)</InputLabel>
-                            <Input id="contributor" placeholder="Last name"/>
+                            <TextField label="Creator" id="creator" rows={3} rowsMax={4} defaultValue={creator}
+                                       multiline={true}/>
                         </FormControl>
                         <FormControl fullWidth mb={3}>
                             <InputLabel htmlFor="Collection Date">
                                 Collection date
                             </InputLabel>
-                            <Input id="collection_date" placeholder="Collection date"/>
+                            <Input id="collection_date" placeholder="Collection date" defaultValue={date}
+                                   multiline={true}/>
                         </FormControl>
                         <FormControl fullWidth mb={3}>
                             <InputLabel htmlFor="language">Language</InputLabel>
-                            <Input id="language" placeholder="Language"/>
+                            <Input id="language" placeholder="Language" defaultValue={language} multiline={true}/>
                         </FormControl>
+                        {owner && (
+                            <Box p={1} bgcolor="grey.300">
+                                <b>Danger Zone(can only be edited by author)</b>
+                                <FormControl fullWidth mb={3}>
+                                    <InputLabel htmlFor="default_target_attribute">default_target_attribute</InputLabel>
+                                    <Input id="default_target_attribute" placeholder="default_target_attribute"
+                                           defaultValue={def_tar_att} multiline={true}/>
+                                </FormControl>
+                                <FormControl fullWidth mb={3}>
+                                    <InputLabel htmlFor="ignore_attribute">ignore_attribute</InputLabel>
+                                    <Input id="ignore_attribute" placeholder="ignore_attribute"
+                                           defaultValue={ignore_att} multiline={true}/>
+                                </FormControl>
+                                <FormControl fullWidth mb={3}>
+                                    <InputLabel htmlFor="row_id_attribute">
+                                        row_id_attribute
+                                    </InputLabel>
+                                    <Input id="row_id_attribute" placeholder="row_id_attribute"
+                                           defaultValue={row_id_att} multiline={true}/>
+                                </FormControl>
+                            </Box>
+                        )}
+
                     </Grid>
                 </Grid>
 
@@ -129,20 +210,6 @@ function Settings() {
                 Edit Dataset
             </Typography>
             <Divider my={3}/>
-
-            <div>
-                <Button color="primary" href="/auth/data-edit">
-                    {" "}
-                    Edit dataset
-                </Button>
-                <Button color="Blue" href="/auth/data-edit-critical">
-                    {" "}
-                    Edit critical fields of a dataset
-                </Button>
-            </div>
-
-            <Divider my={6}/>
-
             <Grid container spacing={6}>
                 <Grid item xs={12}>
                     <Public/>
