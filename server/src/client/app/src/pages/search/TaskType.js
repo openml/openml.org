ae748f2a608f11e1e980d1145d2229520c03979a
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2021-02-10 22:56:42 +0100

    fixes #154

--- server/src/client/app/src/pages/search/TaskType.js
+++ server/src/client/app/src/pages/search/TaskType.js
@@ -1,4 +1,6 @@
 import React from "react";
+import ReactMarkdown from "react-markdown";
+import { Grid, Card, CardContent, Typography } from "@material-ui/core";
 
 export class TaskTypeItem extends React.Component {
   render() {
@@ -8,7 +10,21 @@ export class TaskTypeItem extends React.Component {
           <span className={"fa fa-flag fa-lg"} />
           {this.props.object.name}
         </h1>
-        {this.props.object.description}
+        <Grid item xs={12}>
+          <Card>
+            <CardContent>
+              <Typography variant="h4" mb={6}>
+                Description
+              </Typography>
+              <div className="contentSection">
+                <ReactMarkdown
+                  source={this.props.object.description}
+                  skipHtml
+                />
+              </div>
+            </CardContent>
+          </Card>
+        </Grid>
         <h2>Definition</h2>
         <div className={"subtitle"}>
           Task type visualization not currently supported
