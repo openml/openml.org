a82d31184d36fd7ee1fd8c8470ca01f41d5f23e5
Joaquin Vanschoren <joaquin.vanschoren@gmail.com>
2021-09-20 15:37:15 +0200

    sponsorship pages

--- server/src/client/app/src/pages/docs/About.js
+++ server/src/client/app/src/pages/docs/About.js
@@ -9,16 +9,18 @@ import {
   Paper,
   CardContent,
   Grid,
+  Fab,
   Chip,
+  Zoom,
   Badge,
   List,
   ListItem,
   ListItemText,
   ListItemIcon,
   Avatar,
-  ExpansionPanel,
-  ExpansionPanelSummary,
-  ExpansionPanelDetails,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
   Typography as MuiTypography
 } from "@material-ui/core";
 
@@ -35,6 +37,14 @@ const TopLink = styled(NavHashLink)({
 const Card = styled(MuiCard)(spacing);
 
 const Typography = styled(MuiTypography)(spacing);
+
+const ContactButton = styled(Fab)({
+  margin: 0,
+  top: 85,
+  bottom: 'auto',
+  left: 'auto',
+  position: 'fixed'
+});
 
 const HeroTitle = styled(Typography)({
   textAlign: "center",
@@ -76,7 +86,7 @@ const BigBadge = styled(Badge)`
 `;
 
 const tc_ids = [1, 2, 27, 86, 348, 970];
-const core_ids = [1, 2, 27, 86, 348, 970, 1140, 869, 8111, 9186];
+const core_ids = [1, 2, 27, 86, 348, 970, 1140, 869, 8111, 9186, 3744];
 const active_ids = [10700, 5348, 2902, 8309, 3744];
 const contributor_ids = [1478, 5341];
 
@@ -230,6 +240,19 @@ export default class About extends React.Component {
               Foundation
             </TopLink>
           </List>
+          <Zoom in={true} style={{ transitionDelay: '1000ms'}}>
+            <ContactButton color="primary" size="medium" style={{ right: 16}}
+              href="https://twitter.com/intent/tweet?screen_name=open_ml">
+              <FontAwesomeIcon icon={["fab", "twitter"]} size="lg"/>
+            </ContactButton>
+          </Zoom>
+          <Zoom in={true} style={{ transitionDelay: '1000ms'}}>
+            <ContactButton color="primary" size="medium" 
+                 style={{ backgroundColor: purple[500], right: 76}}
+                 href="https://join.slack.com/t/openml/shared_invite/enQtODg4NjgzNTE4NjU3LTYwZDFhNzQ5NmE0NjIyNmM3NDMyMjFkZDQ0YWZkYWYxMTIxODFmMDhhMTUzMGYzMmM4NjIzYTZlYjBkOGE5MTQ">
+              <FontAwesomeIcon icon={["fab", "slack"]} size="lg"/>
+            </ContactButton>
+          </Zoom>
           <HeroTitle variant="h3" align="center" id="about">
             About OpenML
           </HeroTitle>
@@ -249,42 +272,46 @@ export default class About extends React.Component {
               </Typography>
               <Paragraph>
                 <b>
-                  We believe that machine learning research should be open, but
-                  also well-organized and easily accessible for others to build
-                  on.
+                  We believe that machine learning research should be open,
+                  well-organized and easily accessible to anyone.
                 </b>{" "}
-                Don't you hate it when datasets are hard to find or badly
-                documented, or when results (even your own results) cannot be
-                reproduced later on? Machine learning is a complex process: from
-                preparing data to developing models, a plethora of decisions
-                need to be made. A lot of information gets lost along the way,
-                leading to scattered results, repeated work, and irreproducible
-                models. We aim to create an environment where results from the
-                entire machine learning lifecycle can be easily and accurately
-                shared and organized online, so that anybody can discover,
-                reuse, and learn from them.
+                There is amazing machine learning research going on every day in 
+                labs all over the world. What if we could easily share the latest 
+                great results (datasets, code, experiments) from all of our labs
+                and organize them online, so that everyone can easily discover and 
+                access them, use them in myriad unexpected ways, and solve problems 
+                together on a global scale?
               </Paragraph>
               <Paragraph>
-                <b>
-                  Much like we share open source code to build better software,
-                  we aim to share well-documented machine learning artefacts and
-                  results to do better machine learning, creating a collective
-                  working memory that anyone can tap in to.
-                </b>{" "}
-                Built around open interfaces, OpenML can be used to share
-                results from many different tools and environments, and can be
-                integrated in other systems that use the available datasets and
-                experiments to analyse and learn from them, even in
-                collaborative and/or automated ways.
+                We strive to make as many datasets as possible easily discoverable, well 
+                documented, and frictionlessly accessible. When new models are trained and 
+                evaluated, we also make these results easily discoverable, clearly 
+                documented and reproducible, so that we can trust them, learn from them, 
+                and build on the combined results of the planet. And of course, we use 
+                machine learning on top of all this data to make new discoveries and 
+                automate our work.
               </Paragraph>
               <Typography
                 variant="h6"
                 align="center"
-                style={{ marginBottom: 20 }}
+                style={{ marginBottom: 20, marginTop: 20 }}
               >
                 <ListIcon icon="heart" size="lg" style={{ color: red[400] }} />
                 From the ML community for the ML community
               </Typography>
+              <Paragraph>
+                <b>
+                  We want to build the tools that we ourselves like to 
+                  use, that empower us as researchers, and make our lives easier.
+                </b>{" "}
+                Built around open interfaces, OpenML can be used to <b>automatically</b> share
+                (and import) datasets, algorithms, and experiments results straight from the 
+                tools that we already know and love. Through our APIs, OpenML can be easily 
+                integrated into new workflows and processes, to import new data and 
+                export new results. We also believe in great standards for collecting and 
+                sharing data and for collecting and analysing experimental results.
+              </Paragraph>
+
               <Paragraph>
                 OpenML is being built by an awesome open source community. All
                 code of the OpenML project carries the BSD-3 Clause licence.
@@ -490,13 +517,12 @@ export default class About extends React.Component {
                 style={{ marginBottom: 20 }}
               >
                 <ListIcon icon="hands" size="lg" style={{ color: blue[400] }} />
-                <br /> <b>Our mission</b> <br /> is to make machine learning and
-                data analysis simple, accessible, collaborative and open, to
-                enhance and streamline machine learning research, education and
-                skillful practice.
+                <br /> <b>Our mission</b> <br /> is to make machine learning simple, 
+                accessible, collaborative and open
               </Typography>
               <Paragraph>
-                The foundation aims to achieve this mission by, among others,
+                ... to enhance and streamline machine learning research, education and
+                skillful practice. The foundation aims to achieve this mission by, among others,
                 supporting the OpenML.org platform to offer a worldwide
                 community the means to openly share and build upon each other’s
                 work, enhancing collaboration, education, scholarship, openness
@@ -509,26 +535,10 @@ export default class About extends React.Component {
                 research for the general benefit of society and to create
                 positive outcomes for future generations.
               </Paragraph>
-              <Typography
-                variant="h6"
-                align="center"
-                style={{ marginBottom: 20 }}
-              >
-                <ListIcon
-                  icon="hand-holding-heart"
-                  size="lg"
-                  style={{ color: red[400] }}
-                />
-                <br /> <b>Friends of OpenML</b> <br />
-              </Typography>
-              <Paragraph>
-                Support OpenML by becoming an official partner. Details will
-                follow.
-              </Paragraph>
             </CardContent>
           </Card>
-          <ExpansionPanel>
-            <ExpansionPanelSummary
+          <Accordion>
+            <AccordionSummary
               expandIcon={<FontAwesomeIcon icon="chevron-down" />}
               aria-controls="panel1a-content"
               id="panel1a-header"
@@ -536,8 +546,8 @@ export default class About extends React.Component {
               <Typography>
                 <b>Foundation Board</b>
               </Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
+            </AccordionSummary>
+            <AccordionDetails>
               <Typography>
                 We want to make sure that the money that the foundation receives
                 is invested for the interest of the community. For this purpose,
@@ -547,10 +557,10 @@ export default class About extends React.Component {
                 members are Joaquin Vanschoren, Jan van Rijn, Matthias Feurer,
                 Bernd Bischl, Heidi Seibold, and Giuseppe Casalicchio.
               </Typography>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-          <ExpansionPanel>
-            <ExpansionPanelSummary
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
               expandIcon={<FontAwesomeIcon icon="chevron-down" />}
               aria-controls="panel1a-content"
               id="panel1a-header"
@@ -558,13 +568,13 @@ export default class About extends React.Component {
               <Typography>
                 <b>Annual General Meetings</b>
               </Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
+            </AccordionSummary>
+            <AccordionDetails>
               <Typography>We'll post meeting reports here.</Typography>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-          <ExpansionPanel>
-            <ExpansionPanelSummary
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
               expandIcon={<FontAwesomeIcon icon="chevron-down" />}
               aria-controls="panel2a-content"
               id="panel2a-header"
@@ -572,11 +582,11 @@ export default class About extends React.Component {
               <Typography>
                 <b>Finances</b>
               </Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
+            </AccordionSummary>
+            <AccordionDetails>
               <Typography>We'll post financial overviews here.</Typography>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
+            </AccordionDetails>
+          </Accordion>
         </MainPaper>
       </React.Fragment>
     );
--- server/src/client/app/src/pages/docs/GetInvolved.js
+++ server/src/client/app/src/pages/docs/GetInvolved.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { green, red, blue, grey, purple } from "@material-ui/core/colors";
+import { yellow, green, red, blue, grey, purple } from "@material-ui/core/colors";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   Card as MuiCard,
@@ -8,12 +8,20 @@ import {
   CardContent,
   Grid,
   Chip,
+  Link,
   List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
   Typography as MuiTypography
 } from "@material-ui/core";
 
 import { spacing } from "@material-ui/system";
 import { HashLink } from "react-router-hash-link";
+
+function ListItemLink(props) {
+  return <ListItem button component="a" {...props} />;
+}
 
 const TopLink = styled(HashLink)({
   color: grey[600],
@@ -52,10 +60,38 @@ const ListIcon = styled(FontAwesomeIcon)({
   fontWeight: 800
 });
 
+const ContactLink = ({ link, icon, color, text }) => {
+  return (
+    <ListItemLink button href={link}>
+      <ListItemIcon>
+        <ListIcon icon={icon} size="lg" style={{ color: color }} />
+      </ListItemIcon>
+      <ListItemText>{text}</ListItemText>
+    </ListItemLink>
+  );
+};
+
+const SponsorTier = ({ icon, color, text }) => {
+  return (
+    <ListItemLink button href="about#contact">
+      <ListItemIcon>
+        <ListIcon icon={icon} fixedWidth size="3x" style={{ color: color }} />
+      </ListItemIcon>
+      <ListItemText>{text}</ListItemText>
+    </ListItemLink>
+  );
+};
+
 const ContactChip = ({ link, icon, text }) => {
   return (
     <Chip
-      icon={<ListIcon icon={icon} size="lg" style={{ marginRight: 0 }} />}
+      icon={
+        <ListIcon
+          icon={icon}
+          size="lg"
+          style={{ marginLeft: 10, marginRight: 0 }}
+        />
+      } 
       component="a"
       href={link}
       label={text}
@@ -70,7 +106,7 @@ const ContactChip = ({ link, icon, text }) => {
 export const ContactChipFull = ({ link, icon, text }) => {
   return (
     <Chip
-      icon={<ListIcon icon={icon} size="lg" style={{ marginRight: 0 }} />}
+      icon={<ListIcon icon={icon} size="lg" style={{ marginLeft: 10, marginRight: 0 }} />}
       component="a"
       href={link}
       label={text}
@@ -107,8 +143,8 @@ export default class GetInvolved extends React.Component {
             <TopLink smooth to="/contribute#help">
               Contribute
             </TopLink>
-            <TopLink smooth to="/contribute#support">
-              Support
+            <TopLink smooth to="/contribute#sponsor">
+              Sponsor us
             </TopLink>
           </List>
           <HeroTitle variant="h3" align="center">
@@ -328,17 +364,82 @@ export default class GetInvolved extends React.Component {
               </Card>
             </Grid>
           </Grid>
-          <HeroTitle variant="h3" align="center" id="support">
+          <HeroTitle variant="h3" align="center" id="sponsor">
+            <ListIcon
+              icon="hands-helping"
+              size="lg"
+              style={{ color: blue[400], marginTop: 70 }}
+            />
+            <br />
+            Call for sponsorship
+          </HeroTitle>
+          <Card>
+            <CardContent>
+              <Paragraph>
+                Simply put, without our generous sponsors, OpenML would not be able
+                to make all its resources and services available <b>for free</b> to 
+                the entire world. 
+                We need to support our open source community by running events such 
+                as hackathons, keep our services running, and do a whole lot of development
+                and maintenance work.
+                By donating to OpenML you further the project's mission to democratize
+                machine learning research.
+              </Paragraph>
+              <Paragraph>           
+                Your donations will be used to run community events, which require venues, 
+                catering and thank-you packages, and to maintain and improve our platform services, 
+                which requires server hardware, running costs, as well as technical development
+                and maintenance. Donations go to the not-for-profit <Link href="about#foundation">Open Machine
+                Learning Foundation</Link>.
+              </Paragraph>
+              We are open to many forms of sponsorship, both in kind and in cash. We generally
+              offer the following sponsorship bands, and would love to hear your own wishes
+              about partnering with OpenML.
+              <List component="nav">
+                <SponsorTier
+                  icon={"award"}
+                  color={yellow[800]}
+                  text="Principal – Premium level sponsors of our entire organisation or biggest events. Generally valued at over $400,000 pa in cash and/or contra. Maximum of three sponsors in this category, and must have a contracted term of 3+ years."
+                />
+                <SponsorTier
+                  icon={"award"}
+                  color={grey[400]}
+                  text="Major – Major partners using the sponsorship to achieve a wide range of marketing and business objectives. Generally falling in the range of $100-400,000 pa in cash and/or contra, with a contracted term of 2+ years."
+                />
+                <SponsorTier
+                  icon={"award"}
+                  color={red[800]}
+                  text="Supporting – Lower level sponsors usually using the sponsorship to achieve a more limited number of marketing and business objectives. Generally valued at sub-$100,000 pa in cash and/or contra."
+                />
+              </List>
+            </CardContent>
+          </Card>
+          <HeroTitle variant="h3" align="center" id="sponsor">
             <ListIcon
               icon="hand-holding-usd"
               size="lg"
               style={{ color: green[400], marginTop: 70 }}
             />
             <br />
-            Support us financially
+            Community supporters
           </HeroTitle>
           <Card>
-            <CardContent>We'll set up an OpenCollective page here.</CardContent>
+            <CardContent>
+              <Paragraph>
+                If OpenML is useful to you, or you would like to help us making machine 
+                learning more easily available to everyone, please consider making a donation 
+                via our OpenCollective page. As with <Link href="contribute#sponsor">sponsorships</Link>,
+                we will use your donations to run community events and keep our services free
+                for everyone.
+              </Paragraph>
+              <List component="nav">
+                <SponsorTier
+                  icon={"donate"}
+                  color={yellow[800]}
+                  text="Make a donation on our OpenCollective page. You are awesome!"
+                />
+              </List>
+            </CardContent>
           </Card>
         </MainPaper>
       </React.Fragment>
