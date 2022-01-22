cc227f4a250094081d2b55aa8b23c73659901c77
Joaquin Vanschoren <joaquin.vanschoren@gmail.com>
2022-01-23 00:19:03 +0100

    Simplified structure and big update to node to resolve a string of security issues.

---
+++ server/src/client/app/config-overrides.js
@@ -0,0 +1,20 @@
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
+
+module.exports = {
+    devServer: {
+     stats: {
+       assets: false,
+       chunks: false,
+     }
+    }
+};
+
+module.exports = function override(config, env) {
+    config.plugins.push(new NodePolyfillPlugin({
+        excludeAliases: [
+        ]
+    }));
+    config.resolve.plugins = config.resolve.plugins.filter(plugin => !(plugin instanceof ModuleScopePlugin));
+    return config;
+};
