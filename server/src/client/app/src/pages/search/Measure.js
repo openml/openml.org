bc4be4026f5388bd4fcbcc2df3d0db4a6f9c6dfb
Joaquin Vanschoren <joaquin.vanschoren@gmail.com>
2022-01-26 13:06:56 +0100

    Added styling and markdown rendering for measures. Fixes issue #207

--- server/src/client/app/src/pages/search/Measure.js
+++ server/src/client/app/src/pages/search/Measure.js
@@ -1,18 +1,90 @@
 import React from "react";
+import ReactMarkdown from "react-markdown";
+import remarkMath from 'remark-math'
+import remarkGfm from 'remark-gfm'
+import rehypeKatex from 'rehype-katex'
+import 'katex/dist/katex.min.css'
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
+import {dark} from 'react-syntax-highlighter/dist/esm/styles/prism'
+import {
+  Card,
+  CardContent,
+  Typography,
+  Grid
+} from "@material-ui/core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export class MeasureItem extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <h1 className={"sectionTitle"}>
-          <span className={"fa fa-flag fa-lg"} />
-          {this.props.object.name}
-        </h1>
-        {this.props.object.description}
-        <h2>Definition</h2>
-        <div className={"subtitle"}>
-          Measure visualization not currently supported
-        </div>
+
+        <Grid container spacing={6}>
+          <Grid item xs={12}>
+            <Grid container spacing={6}>
+              <Grid item xs={12}>
+                    <Typography variant="h1" style={{ marginBottom: "15px" }}>
+                      <FontAwesomeIcon icon="tachometer-alt" />
+                      &nbsp;&nbsp;&nbsp;{this.props.object.name}
+                    </Typography>
+              </Grid>
+
+              <Grid item xs={12}>
+                <Card>
+                  <CardContent>
+                    <Typography variant="h4" mb={6}>
+                      Description
+                    </Typography>
+                    <div className="contentSection">
+                      <ReactMarkdown 
+                        children={this.props.object.description}     
+                        remarkPlugins={[remarkMath,remarkGfm]}
+                        rehypePlugins={[rehypeKatex]}
+                        />
+                    </div>
+                  </CardContent>
+                </Card>
+              </Grid>
+              { this.props.object.code &&
+                <Grid item xs={12}>
+                  <Card>
+                    <CardContent>
+                      <Typography variant="h4" mb={6}>
+                        Implementation
+                      </Typography>
+                      <div className="contentSection">
+                        <ReactMarkdown 
+                          children={this.props.object.code}
+                          components={{
+                            code({node, inline, className, children, ...props}) {
+                              const match = /language-(\w+)/.exec(className || '')
+                              return !inline && match ? (
+                                <SyntaxHighlighter
+                                  children={String(children).replace(/\n$/, '')}
+                                  style={dark}
+                                  language={match[1]}
+                                  PreTag="div"
+                                  {...props}
+                                />
+                              ) : (
+                                <SyntaxHighlighter
+                                  children={String(children).replace(/\n$/, '')}
+                                  style={dark}
+                                  PreTag="div"
+                                  {...props}
+                                />
+                              )
+                            }
+                          }}
+                          />
+                      </div>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              }
+            </Grid>
+          </Grid>
+        </Grid>
       </React.Fragment>
     );
   }
