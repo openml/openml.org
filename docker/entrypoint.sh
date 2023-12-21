b8f38c6b3fe3b367d45b5970ed0c088de06ee4cf
Jos van der Velde <j.d.v.d.velde@tue.nl>
2023-12-21 22:09:43 +0100

    Gunicorn access log to stdout and errorlog to stderr, more convenient for Docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-gunicorn -b 0.0.0.0:5000 autoapp:app \
-    --chdir /app \
-    --access-logfile /home/unprivileged-user/access.log \
-    --error-logfile /home/unprivileged-user/error.log
+gunicorn -b 0.0.0.0:5000 autoapp:app --chdir /app
